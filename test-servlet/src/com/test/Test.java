643b0724ad7dbf5580fff7e053f741f7b498adf9
Administrator <Administrator@192.168.230.42>
2018-03-23 14:00:26 +0800

    初始化导入

---
+++ test-servlet/src/com/test/Test.java
@@ -0,0 +1,19 @@
+package com.test;
+
+/**
+ * @author lijun
+ * @version 2.0
+ * @date 2017年4月19日
+ */
+public class Test {
+
+	public final static String ss = "复制qian";
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(ss);
+	}
+
+}
